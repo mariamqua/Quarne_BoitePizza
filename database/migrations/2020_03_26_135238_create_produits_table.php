956cf46023b4c2b0c832e989f90de68f5e2c64ab
mariamquarne <mariam.quarne@edu.uiz.ac.ma>
2020-03-26 17:25:24 +0100

    initial commit

---
+++ database/migrations/2020_03_26_135238_create_produits_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProduitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produits', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nom')->unique();
+            $table->integer('prix');
+            $table->integer('catID');
+            $table->integer('remise');
+            $table->datetime('date_debut');
+            $table->datetime('date_fin');
+            $table->integer('isPromo');
+            $table->text('imgPath');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produits');
+    }
+}
